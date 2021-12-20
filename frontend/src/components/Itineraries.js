fe91e876e99e9b6cf7bd78076c0ada5aca3bd1b4
Nicolas Catriel Cirulli <nicolascirulli2@gmail.com>
2021-12-20 00:52:07 -0300

    Agregado modificar comentarios, falta darle estilo

--- frontend/src/components/Itineraries.js
+++ frontend/src/components/Itineraries.js
@@ -5,6 +5,10 @@ import useDisplay from "../hooks/useDisplay"
 import {useSelector, useDispatch} from 'react-redux'
 import itinerariesActions from "../redux/actions/itinerariesActions";
 import comentariosActions from "../redux/actions/comentariosActions";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 const Itineraries = ({datos}) => {
   const boton = useDisplay();
@@ -13,7 +17,26 @@ const Itineraries = ({datos}) => {
     precio.push(<span>💵</span>);
   }  
 
-  const [btnModificar, setBtnModificar] = useState(false);
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+
+  const [identificador, setIdentificador] = useState(null)
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const modificado = useRef()
 
   const usuarios = useSelector(store => store.usuariosReducer.usuarios)
   const email = useSelector(store => store.usuariosReducer.email)
@@ -22,9 +45,11 @@ const Itineraries = ({datos}) => {
   const token = localStorage.getItem('token')
   const comentario = useRef()
 
-  console.log(btnModificar);
-
-  const handleModificar = () => setBtnModificar(!btnModificar)
+  const modificarComentario = (e) =>{
+    e.preventDefault()
+    dispatch(comentariosActions.modificarComentario(token,datos._id,identificador,modificado.current.value))
+    handleClose()
+  }
 
   const agregarComentario = () => dispatch(comentariosActions.agregarComentarios(token,datos._id,comentario.current.value))
   
@@ -80,9 +105,31 @@ const Itineraries = ({datos}) => {
                     <FiDelete/>
                   </button>
                   {
-                    (<button className="btn-comentario text-primary" onClick={handleModificar}>
-                      <FiEdit/>
-                    </button>)
+                    (
+                      <>
+                      <div>
+                        <Button onClick={handleOpen} className="btn-comentario text-primary"><FiEdit/></Button>
+                        <Modal
+                          open={open}
+                          onClose={handleClose}
+                          aria-labelledby="modal-modal-title"
+                          aria-describedby="modal-modal-description"
+                        >
+                          <Box sx={style}>
+                            <Typography id="modal-modal-title" variant="h6" component="h2">
+                              <form>
+                                <input type='text' placeholder="Comentario a actualizar" ref={modificado} onChange={()=>setIdentificador(comentario._id)}/>
+                                <input type="button" value="enviar" onClick={modificarComentario} />
+                              </form>
+                            </Typography>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                              Editar comentario
+                            </Typography>
+                          </Box>
+                        </Modal>
+                      </div>
+                    </>
+                    )
                     
                   }
                </>
